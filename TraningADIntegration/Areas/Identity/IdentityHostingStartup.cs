bb005a63569450ca6366bae279cc896c3ffd6cda
Sujesh Shrestha <SujeshS@armitgroup.com.au>
2024-09-07 18:01:40 +0930

    Add project files.

---
+++ TraningADIntegration/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TraningADIntegration.Data;
+
+[assembly: HostingStartup(typeof(TraningADIntegration.Areas.Identity.IdentityHostingStartup))]
+namespace TraningADIntegration.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TraningADIntegrationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TraningADIntegrationContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TraningADIntegrationContext>();
+            });
+        }
+    }
+}
